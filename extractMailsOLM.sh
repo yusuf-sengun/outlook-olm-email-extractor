babfb3c10eaafc0819cc97f8de80b1bd8a35dfaa
Yusuf Şengün <75308444+yusuf-sengun@users.noreply.github.com>
2021-05-06 15:05:33 +0300

    Update extractMailsOLM.sh

--- extractMailsOLM.sh
+++ extractMailsOLM.sh
@@ -1,58 +1,84 @@
 #! /bin/bash
-# This sicrip must be located in a folder with olm file(s)
+# This sicript must be located in a folder with olm file(s)
 
 unzip *.olm
 
+extractMails() {
+    while read key; do
+
+        IFS=' ' read -r -a emailArray <<<$key
+
+        for email in "${emailArray[@]}"; do
+            if [[ $email == *"@"* && $email != "@"* && $email != *"telenity"* && $email != *".prod."* && $email != *".PROD."* && $email != *"-prod-"* && $email == *"."* && $email != "xml"* ]]; then
+
+                if [[ $email == *"OPFContactEmailAddressAddress"* ]]; then
+                    email=${email#*=}
+                    email=${email#'"'}
+                    email=${email%'"'}
+                    email=${email#"'"}
+                    email=${email%"'"}
+                    email=${email#'('}
+                    email=${email%')'}
+
+                    echo $email
+                    echo $email >>$outputFile
+                    break
+                fi
+                if [[ $email == *"OPFMessageCopyExchangeConversationId" ]]; then
+                    email=${email%<*}
+                    email=${email%<*}
+                    email=${email#*>}
+                    email=${email#"'"}
+                    email=${email%"'"}
+                    email=${email#'('}
+                    email=${email%')'}
+                    echo $email
+                    echo $email >>$outputFile
+
+                fi
+
+            fi
+        done
+    done <$1
+}
 
 outputFile=allmails.txt
 
-OIFS="$IFS"
-IFS=$'\n'
-for file in `find . -type f -name "m*"`  
-do
-      while read key; do
+if [[ ! -z $1 ]] && [[ ! -z $2 ]]; then
+    lowerDate=$(echo $1 | sed 's/-//g')
+    upperDate=$(echo $2 | sed 's/-//g')
+    echo Email address will be extract between $1 and $2
+    OIFS="$IFS"
+    IFS=$'\n'
+    for file in $(find . -type f -name "m*"); do
+        while read key; do
+            IFS=' ' read -r -a emailArray <<<$key
 
-    IFS=' ' read -r -a emailArray <<< $key
+            for firstWord in "${emailArray[@]}"; do
+                if [[ $firstWord == *"</ExchangeServerLastModifiedTime>"* ]]; then
+                    mailDate=${firstWord#*>}
+                    mailDate=$(echo $mailDate | cut -c 1-10)
+                    mailDate=$(echo $mailDate | sed 's/-//g')
 
-        for email in "${emailArray[@]}"    
-        do     
-            if [[ $email == *"@"* && $email != "@"*  && $email != *".prod."* && $email != *".PROD."* && $email != *"-prod-"* && $email == *"."* && $email != "xml"*  ]]; then 
-
-             
-                if [[ $email == *"OPFContactEmailAddressAddress"* ]]; then 
-                    email=${email#*=} 
-                    email=${email#'"'}
-		    email=${email%'"'}
-		    email=${email#"'"}
-		    email=${email%"'"}
-		    email=${email#'('}
-	            email=${email%')'}
-	            
-                    echo $email
-                    echo $email >> $outputFile
-                    break;
+                    if [[ $mailDate -ge $lowerDate && $mailDate -le $upperDate ]]; then
+                        extractMails $file
+                        
+                    fi
                 fi
-                if [[ $email == *"OPFMessageCopyExchangeConversationId" ]]; then 
-                        email=${email%<*}
-                        email=${email%<*}
-                        email=${email#*>}
-                         email=${email#"'"}
-		    	email=${email%"'"}
-		    	email=${email#'('}
-	            	email=${email%')'}
-                        echo $email 
-                        echo $email  >> $outputFile
+            done
+        done <$file
+    done
+    IFS="$OIFS"
+else
+    OIFS="$IFS"
+    IFS=$'\n'
+    for file in $(find . -type f -name "m*"); do
+        extractMails $file
+    done
+    IFS="$OIFS"
+fi
 
-                fi
-                
-            fi
-        done
-	done < $file
-done
-IFS="$OIFS"
-
-
-sort $outputFile | uniq  > Mails.txt
-sed 's/ \+/,/g' Mails.txt > Mails.csv
-rm -r  $outputFile
+sort $outputFile | uniq >Mails.txt
+sed 's/ \+/,/g' Mails.txt >Mails.csv
+rm -r $outputFile
 
